6cb4f7b118b0cf020e85a315e3ad6ae41f632fe5
Juan David Agudelo <juan.agudelo.aldana@gmail.com>
2023-08-08 09:23:08 -0500

    Update Component Proyecto1.jsx and Styles Proyects.css

--- src/components/Proyecto1.jsx
+++ src/components/Proyecto1.jsx
@@ -22,40 +22,69 @@ export function Proyecto1 () {
                 <h1>Mis Proyectos 😉</h1>
             </Fade>
             <NavbarProyects/>
+            <div className='ContainerImg'>
+                <img alt="" srcSet={Primero}/>
+            </div>
             <div className='ContainerCarousel'>
-                <div className='ContainerImg'>
-                    <img alt="" srcSet={Primero}/>
-                </div>
-                <div className='ContainerDescription'>
-                    <h1><span>Proyecto:</span> Sitio Web de Star Wars con React</h1>
-                    <div className='ContentProyect'>
-                        <h3>Descripción</h3>
-                        <p>
+                <span>Proyecto: </span> 
+                <h1>
+                    Sitio Web de Star Wars con React
+                </h1>
+                <div className='ContentProyect'>
+                    <h3>Descripción</h3>
+                    <p>
                         Este proyecto fue una emocionante oportunidad 
                         para demostrar mis habilidades en el desarrollo 
                         web utilizando React. Diseñé un sitio web 
                         dedicado al universo de Star Wars, que ofrece 
                         información sobre los personajes y planetas de 
                         esta legendaria saga.
-                        </p>
-                        <h3>Tecnologías Utilizadas</h3>
-                        <ul>
-                        <li>
-                            React Bootstrap: Aproveché esta librería para diseñar 
-                            una interfaz atractiva y responsiva, asegurando que el 
-                            sitio luzca bien en diferentes dispositivos.
-                        </li>
-                        <li>
-                            React Router DOM: Implementé la navegación en el sitio, 
-                            permitiendo a los usuarios cambiar entre las páginas de 
-                            personajes y planetas de manera fluida.
-                        </li>
-                        <li>
-                            React Icons: Agregué iconos de Star Wars para darle un 
-                            toque auténtico y resaltar el tema de la saga en toda 
-                            la interfaz.
-                        </li>
-                        </ul>
+                    </p>
+                    <div className="Content">
+                        <div>
+                            <h3>
+                                Tecnologías Utilizadas
+                            </h3>
+                            <ul>
+                                <li>
+                                    React Bootstrap: Aproveché esta librería para diseñar 
+                                    una interfaz atractiva y responsiva, asegurando que el 
+                                    sitio luzca bien en diferentes dispositivos.
+                                </li>
+                                <li>
+                                    React Router DOM: Implementé la navegación en el sitio, 
+                                    permitiendo a los usuarios cambiar entre las páginas de 
+                                    personajes y planetas de manera fluida.
+                                </li>
+                                <li>
+                                    React Icons: Agregué iconos de Star Wars para darle un 
+                                    toque auténtico y resaltar el tema de la saga en toda 
+                                    la interfaz.
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>
+                                Funcionalidades Clave
+                            </h3>
+                            <ul>
+                                <li>
+                                    <span>Exploración de Personajes: </span>
+                                    Los usuarios pueden acceder a una lista completa de 
+                                    personajes de Star Wars, donde se muestra información 
+                                    detallada sobre cada uno, incluyendo sus habilidades, 
+                                    afiliaciones y apariciones en la saga.
+                                </li>
+                                <li>
+                                    <span>Descubrimiento de Planetas: </span>
+                                    El sitio ofrece una página dedicada a los planetas, 
+                                    donde los usuarios pueden explorar diferentes mundos, 
+                                    acompañados de datos relevantes, como el clima, 
+                                    terreno, población y eventos importantes que 
+                                    ocurrieron en cada planeta.
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
