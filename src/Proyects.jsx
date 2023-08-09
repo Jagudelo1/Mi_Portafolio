009330a7dd485afc71cdf314dc90712397dd640d
Juan David Agudelo <93266129+Jagudelo1@users.noreply.github.com>
2023-08-09 10:04:04 -0500

    Update Proyects

--- src/Proyects.jsx
+++ src/Proyects.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import './css/Proyects.css';
-import Logo from './assets/MiLogo.gif';
-import { Zoom } from 'react-awesome-reveal';
-import { Link } from 'react-router-dom';
 import { MisRedes } from './components/MisRedes';
 import { NavbarProyects } from './components/NavbarProyects';
 import { FooterApp } from "./components/FooterApp";
+import { ReturnApp } from "./components/ReturnApp";
 import ApiReact from './assets/Api_StarWars/1.png';
+
+
 
 export function Proyects() {
     return(
         <div class="ContainerProyects">
-            <Zoom className="Logo">
-                <Link to="/">
-                    <img src={Logo} alt="" />
-                </Link>
-            </Zoom>
+            <ReturnApp/>
             <MisRedes/>
             <NavbarProyects/>
+
             <div>
-                <div>
-                    <img src={ApiReact} alt="Proyecto1" />
+                <div className="ImageProyects">
+                    <img src={ApiReact} alt="" />
                 </div>
-                <p>En esta sección encontraras mis proyectos realizados personal
-                    y academicos
+                <p>
+                    En esta sección encontraras mis proyectos realizados personal
+                    y academicos, lamentablemente los proyectos no estan alojados
+                    en un hosting por temas academicos, pero se puede observar en
+                    dandole clic en uno de los proyectos donde encontran capturas
+                    e información correspondiente a cada uno de los proyectos.
                 </p>
             </div>
             <FooterApp/>
