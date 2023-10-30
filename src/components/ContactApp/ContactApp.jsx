aa3df9ed380a510dbc1eae3b3fdae3414ed0b2c4
Juan David Agudelo <93266129+Jagudelo1@users.noreply.github.com>
2023-10-30 10:01:43 -0500

    Actualización de alerta

--- src/components/ContactApp/ContactApp.jsx
+++ src/components/ContactApp/ContactApp.jsx
@@ -2,70 +2,106 @@ import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
 import '../../css/ContactApp.css';
 
 export function ContactApp() {
     const [name, setName] = useState('');
     const [from_email, setFrom_Email] = useState('');
     const [message, setMessage] = useState('');
+    const [acceptTerms, setAcceptTerms] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    
+
+        if (!acceptTerms) {
+            // Mostrar una alerta de error si los términos no se aceptan
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Debes aceptar los términos y condiciones.',
+            });
+            return;
+        }
+
         emailjs.send('service_hyhpiod', 'template_b5w1ile', {
             from_name: name,
-            from_email: from_email, // Utiliza el correo ingresado en el formulario como remitente
+            from_email: from_email,
             message: message,
         }, 'o7ZvxwY6Lz0WruG7j')
         .then((response) => {
-            console.log('Correo enviado con éxito', response);
+            console.log('Mensaje enviado con éxito', response);
+
+            // Mostrar una alerta de éxito
+            Swal.fire({
+                icon: 'success',
+                title: '¡Éxito!',
+                text: 'El mensaje se envió con éxito.',
+            });
         }, (error) => {
-            console.error('Error al enviar el correo', error);
+            console.error('Error al enviar el mensaje', error);
+
+            // Mostrar una alerta de error
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Hubo un error al enviar el mensaje. Por favor, inténtalo de nuevo.',
+            });
         });
-    
+
         // Limpiar el formulario
         setName('');
         setFrom_Email('');
         setMessage('');
+        setAcceptTerms(false);
     };
 
-    return(
+    return (
         <>
             <div className="ContactForm">
                 <Form className="Formulario">
                     <Form.Group className="mb-3 Nombre">
-                        <Form.Control 
-                        type="text" 
-                        placeholder="Nombre"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)} />
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="Nombre"
+                            name="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-3 Correo">
-                        <Form.Control 
-                        type="text" 
-                        placeholder="Correo" 
-                        value={from_email}
-                        onChange={(e) => setFrom_Email(e.target.value)}/>
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="Correo"
+                            name="from_email"
+                            value={from_email}
+                            onChange={(e) => setFrom_Email(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-3 textarea">
-                        <Form.Control 
-                        as="textarea" 
-                        rows={3} 
-                        placeholder="Asunto" 
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}/>
+                        <Form.Control
+                            required
+                            as="textarea"
+                            rows={3}
+                            placeholder="Comentario"
+                            name="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Check
+                            required
                             className="swicht"
                             type="switch"
                             id="custom-switch"
+                            name="acceptTerms"
                             label="Aceptar términos y condiciones"
-                        />
+                            checked={acceptTerms}
+                            onChange={(e) => setAcceptTerms(e.target.checked)} />
                     </Form.Group>
-                    <Button 
+                    <Button
                         className="ButtonSub"
-                        type="submit" 
+                        type="submit"
                         onClick={handleSubmit}>
                         Enviar
                     </Button>
