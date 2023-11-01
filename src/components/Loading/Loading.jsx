27160e933215af1055d9bae6bfbd2c660add2bfa
Juan David Agudelo <93266129+Jagudelo1@users.noreply.github.com>
2023-11-01 12:24:18 -0500

    .

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ import { Fade } from "react-awesome-reveal";
 
 export function Loading() {
     return(
-        <>
+        <div className="ContentContainer">
             <Fade className="ContentImg">
                 <img src={Carga} alt="Cargando..." />
             </Fade>
@@ -13,6 +13,6 @@ export function Loading() {
                 <h2 className='Cargando'>Cargando...</h2>
                 <h2 className='Cargando2'>Cargando...</h2>
             </Fade>
-        </>
+        </div>
     )
 }
